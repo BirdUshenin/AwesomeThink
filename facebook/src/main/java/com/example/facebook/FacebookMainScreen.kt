4a7e4008d3c1c16ef6fc63d184054054a29b07c0
uiv <uiv@coolclever.ru>
2024-03-14 13:15:51 +0300

    State && Visibility

--- facebook/src/main/java/com/example/facebook/FacebookMainScreen.kt
+++ facebook/src/main/java/com/example/facebook/FacebookMainScreen.kt
@@ -1,22 +1,66 @@
 package com.example.facebook
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 
+@Preview
 @Composable
-fun MainScreen(){
+fun MainScreen() {
+    val snackBarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(
+                    onClick = {
+                        scope.launch {
+                            val snackAction = snackBarHostState.showSnackbar(
+                                message = "This is PARTA",
+                                actionLabel = "Hide FAB",
+                                duration = SnackbarDuration.Long
+                            )
+                            if (snackAction == SnackbarResult.ActionPerformed) {
+                                fabIsVisible.value = false
+                            }
+                        }
+                    }
+                ) {
+                    Icon(
+                        Icons.Filled.Favorite,
+                        contentDescription = null
+                    )
+                }
+            }
+        },
         bottomBar = {
             NavigationBar {
                 val selectedPosition = remember {
